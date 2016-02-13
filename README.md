4edc32e41e575f3b8eb92df1cb4091e5275d74db
Sae Vang <sensaetions@gmail.com>
2016-02-12 18:30:21 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Workflows with Gulp
+
+Creating workflow using Gulp task runner
---
+++ builds/development/index.php
@@ -0,0 +1,12 @@
+<?php
+require_once('shared/header.php');
+?>
+
+    <main class="main" role="main">
+        <h1>Lorem Ipsum</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum eveniet beatae, aliquid pariatur excepturi voluptates reiciendis architecto. Assumenda temporibus incidunt tempore vero obcaecati, voluptatibus hic, soluta, deserunt tempora voluptatem unde.</p>
+    </main>
+
+    <?php
+require_once('shared/footer.php');
+?>
